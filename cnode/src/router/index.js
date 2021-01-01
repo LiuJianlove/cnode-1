c077e3eaf40dedb1dddd3a04fc80cb5470f35076
Stephen <1137657002@qq.com>
2021-01-01 20:24:56 +0800

    first commit

---
+++ cnode/src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import PostList from '../components/PostList'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      name: 'root',
+      path: '/',
+      components: {
+        main: PostList
+      }
+    }
+    
+  ]
+})
